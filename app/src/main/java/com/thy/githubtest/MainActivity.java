0469993e28c9f5caf6f3b27e0e231f24b4c66dbb
thyun <thyun85@gmail.com>
2018-04-26 14:34:40 +0900

    first commit

--- app/src/main/java/com/thy/githubtest/MainActivity.java
+++ app/src/main/java/com/thy/githubtest/MainActivity.java
@@ -2,12 +2,18 @@ package com.thy.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+        tv.setText("Hello world");
     }
 }
